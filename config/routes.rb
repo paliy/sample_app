cf2e6a8e0d472af9b04cd35ee1539c423a0bc73b
paliy <georgi.paliy@gmail.com>
2014-08-21 17:57:54 +0300

    sign problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 SampleApp::Application.routes.draw do
-  resources :users	
-  # get "users/new"
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]	
   root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
  
-  # get "static_pages/users/new"	 
+
+  # get "users/new"
+  # get "static_pages/users/new"   
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
   # get "static_pages/contact"
-
-
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
