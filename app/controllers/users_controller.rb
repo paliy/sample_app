1bc2dec082f91eca33b8e528b8083dc5b15961fc
paliy <georgi.paliy@gmail.com>
2014-08-22 17:50:54 +0300

    edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,68 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
+	before_action :signed_in_user, only: [:index, :edit, :update]
+	before_action :correct_user,   only: [:edit, :update]
 
-  def show
-    @user = User.find(params[:id])
-  end
 
-  def create
-    @user = User.new(user_params)    # Not the final implementation!
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"	
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+
+	def edit
+    	# @user = User.find(params[:id])
+    end
+
+
+	def new
+		@user = User.new
+	end
+
+
+	def show
+    	@user = User.find(params[:id])
+	end
+
+
+	def create
+    	@user = User.new(user_params)    # Not the final implementation!
+    	if @user.save
+      		flash[:success] = "Welcome to the Sample App!"	
+      		redirect_to @user
+    	else
+      		render 'new'
+    	end
+    end
+
+
+   def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
-  end
+   end
+
 
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user=(user)
     @current_user = user
   end
@@ -26,5 +30,15 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
 
 end
